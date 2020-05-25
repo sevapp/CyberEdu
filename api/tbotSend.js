53a1172c1180d65a70bb59144fc8af83c69d1bb6
Vsevolod Pletnev <mr.sevapp@gmail.com>
2020-05-25 21:05:08 +0300

    add bot

--- api/tbotSend.js
+++ api/tbotSend.js
@@ -1,7 +1,21 @@
+const https = require('https');
+
 module.exports = (req, res) => {
-    res.json({
-        body: req.body,
-        query: req.query,
-        cookies: req.cookies
-    })
+
+    const options = {
+        hostname: 'https://api.telegram.org',
+        port: 443,
+        path: `/bot${process.env.BOT_KEY}/sendMessage?chat_id=${process.env.CHAT_ID}&text=${req.query.msg}`,
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': data.length
+        }
+    }
+
+    https.get(options, (response) => {
+        res.json({
+            result: response,
+        })
+    });
 }

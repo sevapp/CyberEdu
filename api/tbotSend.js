712de0226f97c5d8b137db3556ca5da1a81f67e2
Vsevolod Pletnev <mr.sevapp@gmail.com>
2020-05-26 18:04:34 +0300

    rebuild

--- api/tbotSend.js
+++ api/tbotSend.js
@@ -1,11 +1,11 @@
 const https = require('https');
 
-const telegram = 'https://api.telegram.org';
-const bot = `bot${process.env.BOT_KEY}`;
-const chat = `chat_id=${process.env.CHAT_ID}`;
+const tAPI = 'https://api.telegram.org';
+const botToken = `bot${process.env.BOT_KEY}`;
+const chatID = `chat_id=${process.env.CHAT_ID}`;
 
 module.exports = (req, res) => {
-    let token = req.query.token || null;
+    let capchaToken = req.query.token || null;
     let name = req.query.name || null;
     let phone = req.query.phone || null;
     let msg = req.query.msg || null;
@@ -16,7 +16,7 @@ module.exports = (req, res) => {
         тел. ${phone}\n
     `;
 
-    let link = `${telegram}/${bot}/sendMessage?${chat}&text=${text}`;
+    let link = `${tAPI}/${botToken}/sendMessage?chatID}&text=${text}`;
 
     https.get(link, (response) => {
         res.json({

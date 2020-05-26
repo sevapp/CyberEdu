1cce06874937dc32e4cdc8bff259c57c3d7f89c2
Vsevolod Pletnev <mr.sevapp@gmail.com>
2020-05-26 23:49:55 +0300

    debug phone

--- api/tbotSend.js
+++ api/tbotSend.js
@@ -7,7 +7,7 @@ const chatID = `chat_id=${process.env.CHAT_ID}`;
 module.exports = (req, res) => {
     let capchaToken = req.query.token || null;
     let name = req.query.name || 'null';
-    let phone = req.query.phone.replace('+', '%2b') || 'null';
+    let phone = req.query.phone || 'null';
     let msg = req.query.msg || 'null';
 
     let text = `Новая заявка от ${name}:%0A${msg}%0AТелефон: ${phone}`;

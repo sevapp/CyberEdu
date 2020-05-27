bd024f2339cb6e17c0c3a79f4704e0c4b74dcbe1
Vsevolod Pletnev <mr.sevapp@gmail.com>
2020-05-27 13:12:11 +0300

    add capcha on back

--- api/tbotSend.js
+++ api/tbotSend.js
@@ -1,21 +1,24 @@
 const https = require('https');
 
-const tAPI = 'https://api.telegram.org';
-const botToken = `bot${process.env.BOT_KEY}`;
-const chatID = `chat_id=${process.env.CHAT_ID}`;
+const capchaAPI   = 'https://www.google.com/recaptcha/api/siteverify';
+const capchaSecret = `bot${process.env.CAPCHA_PRT}`;
+
+const botAPI    = 'https://api.telegram.org';
+const botToken  = `bot${process.env.BOT_KEY}`;
+const chatID    = `chat_id=${process.env.CHAT_ID}`;
 
 module.exports = (req, res) => {
     let capchaToken = req.query.token || null;
-    let name = req.query.name || 'null';
-    let phone = req.query.phone || 'null';
-    let msg = req.query.msg || 'null';
+    let caphcaSendLink = `${capchaAPI}/secret=${capchaSecret}/response=${capchaToken}`;
 
+    let name  = req.query.name  || 'null';
+    let phone = req.query.phone || 'null';
+    let msg   = req.query.msg   || 'null';
     let text = `Новая заявка от ${name}:%0A${msg}%0AТелефон: ${phone}`;
-    let link = `${tAPI}/${botToken}/sendMessage?${chatID}&text=${text}`;
-    
-    https.get(link, (response) => {
-        res.json({
-            result: link,
-        });
+    let botSendLink = `${botAPI}/${botToken}/sendMessage?${chatID}&text=${text}`;
+
+    https.get(caphcaSendLink, (response) => {
+        res.json(response);
+        https.get(botSendLink, (response) => {});
     });
 };

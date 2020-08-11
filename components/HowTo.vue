7e249f3924dfe10f4f4a23e1135fecc5becd7e4f
Vsevolod Pletnev <mr.sevapp@gmail.com>
2020-08-11 18:24:27 +0300

    remove comments

--- components/HowTo.vue
+++ components/HowTo.vue
@@ -48,21 +48,6 @@
 <style lang="scss" module>
 
 .section {
-  // margin: 0 auto;
-  // width: 100%;
-  // // min-height: 100vh;
-  // display: flex;
-  // justify-content: center;
-  // align-items: center;
-  // text-align: center;
-  // flex-direction: column;
-
-  // @media (orientation: portrait) {
-  //   // flex-direction: column-reverse;
-  //   // min-height: inherit;
-  //   padding: 0;
-  // }
-  
   padding-top: 5.5rem;
   // padding-bottom: 1.5rem;
 
@@ -74,33 +59,15 @@
 }
 
 .Title {
-	// margin: 0 4rem 0 0;
-	// width: 40%;
-	// min-height: 26rem;
-	// display: flex;
-	// justify-content: center;
-  // align-items: center;
-  // padding: 1rem;
-	//text-align: left;
   font-size: 4.5rem;
-  // hyphens: auto;
   color: #666666;
-  // text-shadow:
-  //   -1rem -1rem 0 #ffffff,
-  //   1rem -1rem 0 #ffffff,
-  //   -1rem 1rem 0 #ffffff,
-  //   1rem 1rem 0 #ffffff;
   line-height: 1.1;
   user-select: none;
-  // word-break: break-all;
 
 	@media (orientation: portrait) {
 		margin: 0rem 1rem 0rem 1rem;
     font-size: 2.6rem;
     text-align: left;
-    // min-width: 100%;
-    // min-height: 28rem;
-    // text-align: center;
 	}
 }
 
@@ -128,22 +95,16 @@
 }
 
 li {
-  // background: #ffdd57;
-  // padding: 1rem;
   border-radius: 4px;
   margin-left: -10px;
   line-height: 1.3;
   background: #fff;
   border: 5px solid #666666;
   padding: 1.5rem;
-  // text-decoration: underline;
 
   @media (orientation: portrait) {
     padding: 1rem;
     margin-left: 0;
-    // border: none;
-    // box-shadow: rgba(0, 0, 0, 0.1) 4px 4px;
-    // text-shadow: rgba(0, 0, 0, 0.1) 4px 4px;
   }
 }
 
